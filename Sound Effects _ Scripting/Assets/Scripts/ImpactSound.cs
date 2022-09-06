9710956bd5877a419e2631b266ef277f0dacebbc
soundkamera <81432720+soundkamera@users.noreply.github.com>
2022-09-06 09:19:55 -0400

    added unity project

---
+++ Sound Effects _ Scripting/Assets/Scripts/ImpactSound.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImpactSound : MonoBehaviour
+{
+    public AudioClip impactSoft;
+    public AudioClip impactHard;
+
+    private AudioSource source;
+    private float lowPitchRange = .75F;
+    private float highPitchRange = 1.25F;
+    private float velToVol = .2F;
+    private float velocityClipSplit = 10F;
+
+    void Awake()
+    {
+        source = GetComponent<AudioSource>();  
+    }
+
+    private void OnCollisionEnter(Collision coll)
+
+    {
+        source.pitch = Random.Range(lowPitchRange, highPitchRange);
+        float hitVol = coll.relativeVelocity.magnitude * velToVol;
+        if (coll.relativeVelocity.magnitude < velocityClipSplit)
+            source.PlayOneShot(impactSoft, hitVol);
+        else
+            source.PlayOneShot(impactHard, hitVol);
+
+    }
+
+}
---
+++ Sound Effects _ Scripting/Assets/Scripts/ThrowObject.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrowObject : MonoBehaviour
+{
+    public GameObject projectile;
+    public AudioClip shootSound;
+
+    private float throwSpeed = 2000f;
+    private AudioSource source;
+    private float volLowRange = .5f;
+    private float volHighRange = 1.0f;
+
+  
+    void Awake()
+    {
+        source = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown ("Fire1"))
+        {
+            float vol = Random.Range(volLowRange, volHighRange);
+            source.PlayOneShot(shootSound,vol);
+            GameObject throwThis = Instantiate(projectile, transform.position, transform.rotation) as GameObject;
+            throwThis.GetComponent<Rigidbody>().AddRelativeForce(new Vector3(0, 0, throwSpeed));
+
+        }
+        
+    }
+}
